c7ac78d481692a422c61cb7e1ce2ad31d07b0925
lixiaopeng <dragonflylxp@gmail.com>
2018-06-13 11:04:30 +0800

    一个解析redis aof文件的脚本
    将aof文件格式解析成redis命令集合

---
+++ parse_aof.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+""" A redis appendonly file parser
+"""
+
+import hiredis
+import sys
+
+if len(sys.argv) != 2:
+    print sys.argv[0], 'aof_file'
+    sys.exit()
+
+with open(sys.argv[1]) as f:
+    line = f.readline()
+    cur_request = line
+    while line:
+        req_reader = hiredis.Reader()
+        req_reader.setmaxbuf(0)
+        req_reader.feed(cur_request)
+        command = req_reader.gets()
+        try:
+            if command is not False:
+                print command
+                cur_request = ''
+        except hiredis.ProtocolError:
+            print 'protocol error'
+        line = f.readline()
+        cur_request += line
