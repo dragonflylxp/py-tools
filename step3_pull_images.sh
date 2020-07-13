1f0d5a5be7495b99d41d194aad307e07f402d4ac
lixiaopeng <dragonflylxp@gmail.com>
2020-07-13 11:05:18 +0800

    docker pull images: nginx/mariadb/php

---
+++ step3_pull_images.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+echo '>>>[PULL IMAGES]docker pull nginx'
+docker pull nginx 
+
+echo '>>>[PULL IMAGES]docker pull mariadb'
+docker pull mariadb 
+
+echo '>>>[PULL IMAGES]docker pull mariadb'
+docker pull php 
