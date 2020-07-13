321458b8d2803a06f04e11276123610dd5b28d95
lixiaopeng <dragonflylxp@gmail.com>
2020-07-13 16:56:10 +0800

    pull images: nginx/mysql/php

--- step3_pull_images.sh
+++ step3_pull_images.sh
@@ -4,8 +4,8 @@
 echo '>>>[PULL IMAGES]docker pull nginx'
 docker pull nginx 
 
-echo '>>>[PULL IMAGES]docker pull mariadb'
+echo '>>>[PULL IMAGES]docker pull mysql'
 docker pull mariadb 
 
-echo '>>>[PULL IMAGES]docker pull mariadb'
-docker pull php 
+echo '>>>[PULL IMAGES]docker pull php'
+docker pull php:7.1-fpm
